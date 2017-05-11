dcb038d0c1f5b2f3feedc411fd0f7473fb47eb6a
Ludo <vcs@ludo.co.za>
2017-05-11 22:25:28 +0200

    gem deps versions

--- install.ubuntu.12.04.sh
+++ install.ubuntu.12.04.sh
@@ -21,17 +21,31 @@ echo "Create cit database, mysql root password needed..."
 echo "CREATE DATABASE cit DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci; GRANT ALL ON cit.* TO 'cit'@'localhost' IDENTIFIED BY 'cit'; FLUSH PRIVILEGES;" | mysql -u root -p
 
 echo "Install needed ruby gems..."
-gem install gchartrb RedCloth json tzinfo echoe fastercsv test-spec ferret hoe mongrel eventmachine -r
+gem install json -v 1.8.6 -r
+gem install json_pure -v 2.0.1 -r
+gem install rdoc -v 4.2.2 -r
+gem install rake -v 10.4.2 -r
+gem install gchartrb RedCloth tzinfo echoe fastercsv test-spec ferret hoe mongrel eventmachine -r
+# To lazy now but we will need this in the future:
+# gchartrb-0.8
+# RedCloth-4.3.2
+# tzinfo-1.2.3
+# echoe-4.6.6
+# fastercsv-1.5.5
+# test-spec-0.10.0
+# ferret-0.11.8.7
+# hoe-3.16.0
+# mongrel-1.1.5
+# eventmachine-1.2.3
 gem install ZenTest -v 4.1.4 -r
 gem install icalendar -v 1.1.0 -r
 gem install mysql -v 2.7 -r
+
 
 echo "Moving cit directory to /opt/cit..."
 cd $SCRIPT_DIR
 mv cit /opt/.
 cd /opt/cit
-echo "Create log directory..."
-mkdir -p log
 
 echo "Setup ruby app..."
 ruby setup.rb
